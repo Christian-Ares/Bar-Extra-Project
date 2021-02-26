c8a2605caca2a75e9118132dc8e1b609c06f722a
Christian Ares <christianaresvillan@gmail.com>
2021-02-26 19:49:51 +0100

    Social Links added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ export const Footer = () => {
     return (
         <div>
             <p>Coded and build by <strong>Christian Ares</strong></p>
+            <a href="https://www.linkedin.com/in/christian-ares/">Linkedin</a>
         </div>
     )
 }
