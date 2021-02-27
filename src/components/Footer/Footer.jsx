3134ff0083b9d86d4a8c9f16590dce7bc93e5d2f
Christian Ares <christianaresvillan@gmail.com>
2021-02-27 21:56:03 +0100

    Footer changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,8 +7,8 @@ export const Footer = () => {
                 <p>Coded and build by <strong>Christian Ares</strong></p>
                     <div className="links">
                         <a href="https://www.linkedin.com/in/christian-ares/">Linkedin</a>
-                        <a href="#">Twitter</a>
-                        <a href="#">Instagram</a>
+                        <a href="jhttps://www.twitter.com">Twitter</a>
+                        <a href="https://www.instagram.com">Instagram</a>
                     </div>
             </footer>
         </div>
