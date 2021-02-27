20ee9cd7c4da4018b9d4c56ab9fb6b93f612bb9d
Christian Ares <christianaresvillan@gmail.com>
2021-02-27 19:19:38 +0100

    provisional footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,8 +3,12 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div>
-            <p>Coded and build by <strong>Christian Ares</strong></p>
-            <a href="https://www.linkedin.com/in/christian-ares/">Linkedin</a>
+            <footer>
+                <p>Coded and build by <strong>Christian Ares</strong></p>
+                <a href="https://www.linkedin.com/in/christian-ares/">Linkedin</a><div></div>
+                <a href="#">Twitter</a><div></div>
+                <a href="#">Instagram</a><div></div>
+            </footer>
         </div>
     )
 }
