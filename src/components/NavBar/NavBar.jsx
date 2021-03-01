0be735c8a454b4d6d0d742697d1589fbb410ec86
Christian Ares <christianaresvillan@gmail.com>
2021-03-01 21:27:19 +0100

    NavBar Changed

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,21 +3,20 @@ import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 export const NavBar = () => {
 	return (
-	
-		<Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Navbar.Brand href="#home">BarExtra</Navbar.Brand>
-					<Nav className="mr-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Button variant="outline-info">Acceso Empresa</Button>
-						<Button variant="outline-info">Acceso Candidato</Button>
-					</Nav>
-					<Form inline>
-						<FormControl type="text" placeholder="" className="mr-sm-2" />
-						<Button variant="outline-info">Buscar</Button>
-					</Form>
-			</Navbar.Collapse>
-		</Navbar>
+	<Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+		<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+		<Navbar.Collapse id="responsive-navbar-nav">
+			<Navbar.Brand href="#home">BarExtra</Navbar.Brand>
+				<Nav className="mr-auto">
+					<Nav.Link href="#home">Home</Nav.Link>
+					<Button variant="outline-info">Acceso Empresa</Button>
+					<Button variant="outline-info">Acceso Candidato</Button>
+				</Nav>
+				<Form inline>
+					<FormControl type="text" placeholder="" className="mr-sm-2" />
+					<Button variant="outline-info">Buscar</Button>
+				</Form>
+		</Navbar.Collapse>
+	</Navbar>
 	)
 }
