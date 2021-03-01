2fb7729b02b5909a20476ae46f8b5498a7d79849
Christian Ares <christianaresvillan@gmail.com>
2021-03-01 21:27:56 +0100

    Footer changed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ export const Footer = () => {
 	return (
 		<div className="footer">
 			<footer>
-							<p>Coded and build by <strong>Christian Ares</strong></p>
-								<div className="links">
-												<a href="https://www.linkedin.com/in/christian-ares/">Linkedin</a>
-												<a href="jhttps://www.twitter.com">Twitter</a>
-												<a href="https://www.instagram.com">Instagram</a>
-								</div>
+			<p>Coded and build by <strong>Christian Ares</strong></p>
+				<div className="links">
+					<a href="https://www.linkedin.com/in/christian-ares/">Linkedin</a>
+					<a href="jhttps://www.twitter.com">Twitter</a>
+					<a href="https://www.instagram.com">Instagram</a>
+				</div>
 			</footer>
 		</div>
 	)
